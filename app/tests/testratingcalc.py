9dbac398e85c581d25e81df17744e9fac955d98d
Siyu-SG <gongsiyu040627@gmail.com>
2021-09-27 19:22:51 -0500

    Update testratingcalc.py

--- app/tests/testratingcalc.py
+++ app/tests/testratingcalc.py
@@ -1,2 +1,3 @@
 # Use unittest library to create a unit test method to test the calc_avg_rating function created in the ratingscalc module
 # Make sure you include multiple unit tests to test that the function acts as expected in different scenarios
+# Test editing the document
